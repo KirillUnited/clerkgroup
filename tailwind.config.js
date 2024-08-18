047b0d6ace3373dfb594f09dd836425e6175a794
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-18 12:29:49 +0300

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,njk}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '20px'
+    },
+    fontFamily: {
+      primary: ['var(--font-primary)', ...fontFamily.sans],
+      foreground: ['var(--font-foreground)', ...fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        text: 'var(--text)'
+      }
+    },
+  },
+  plugins: [],
+}
+
