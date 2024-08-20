7f1db6c58dc1d1478a26ae2c2191dbec45626919
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-21 01:22:42 +0300

    Add mobile menu

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,10 @@
 import "../styles/app.scss";
-// import "./pricing-slider"
 import { handleMediaQueryChange, mediaQuery } from "./pricing-slider";
 import "./custom-select";
+import MobileMenu from "./mobile-menu";
 
 window.addEventListener('DOMContentLoaded', () => {
     handleMediaQueryChange(mediaQuery);
     mediaQuery.addEventListener('change', handleMediaQueryChange);
+    new MobileMenu({});
 })
