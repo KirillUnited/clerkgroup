9e45e5e1933dc73e3d7087a8f1e184c01c7aa021
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-20 18:07:03 +0300

    Update mobile pricing list

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,16 +1,29 @@
 import "../styles/app.scss";
 
-const swiper = new Swiper('.swiper', {
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    centeredSlides: true,
-    initialSlide: 1,
-    breakpoints: {
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        centeredSlides: false,
-      },
-    },
-});
+let swiper = new Swiper('.swiper', {
 
+});
+const mediaQuery = window.matchMedia('(max-width: 1024px)');
+const handleMediaQueryChange = (e) => {
+    if (e.matches) {
+        swiper = new Swiper('.swiper', {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            centeredSlides: true,
+            initialSlide: 1,
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                    centeredSlides: false,
+                },
+            },
+        });
+    } else {
+        swiper.destroy();
+    }
+}
+window.addEventListener('DOMContentLoaded', () => {
+    handleMediaQueryChange(mediaQuery);
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+})
