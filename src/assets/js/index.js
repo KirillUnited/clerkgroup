620013de77d62825929566a4744ff7f11c63f905
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-20 16:38:50 +0300

    Add Swiper

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,16 @@
 import "../styles/app.scss";
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    centeredSlides: true,
+    initialSlide: 1,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        centeredSlides: false,
+      },
+    },
+});
+
