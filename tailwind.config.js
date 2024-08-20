8402966edf8ea04c28b2dce6cf4cf91de7272740
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-20 19:02:37 +0300

    Update container screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '20px'
+      padding: '20px',
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1120px',
+      },
     },
     fontFamily: {
       primary: ['var(--font-primary)', ...fontFamily.sans],
