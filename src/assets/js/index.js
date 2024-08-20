c9c6338260bbba5ec0e63eb0f8dcff47eead15bc
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-20 21:05:55 +0300

    Fixes

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,28 +1,7 @@
 import "../styles/app.scss";
+import "./pricing-slider"
+import { handleMediaQueryChange, mediaQuery } from "./pricing-slider";
 
-let swiper = new Swiper('.swiper', {
-
-});
-const mediaQuery = window.matchMedia('(max-width: 1024px)');
-const handleMediaQueryChange = (e) => {
-    if (e.matches) {
-        swiper = new Swiper('.swiper', {
-            slidesPerView: "auto",
-            spaceBetween: 20,
-            centeredSlides: true,
-            initialSlide: 1,
-            breakpoints: {
-                1024: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
-                    centeredSlides: false,
-                },
-            },
-        });
-    } else {
-        swiper.destroy();
-    }
-}
 window.addEventListener('DOMContentLoaded', () => {
     handleMediaQueryChange(mediaQuery);
     mediaQuery.addEventListener('change', handleMediaQueryChange);
