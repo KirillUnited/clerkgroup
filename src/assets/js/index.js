a3c5f2a5ef4378e9cdf52353d30cd11fbc262ba5
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-20 23:51:14 +0300

    Add select plugin

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 import "../styles/app.scss";
 import "./pricing-slider"
 import { handleMediaQueryChange, mediaQuery } from "./pricing-slider";
+import "./custom-select";
 
 window.addEventListener('DOMContentLoaded', () => {
     handleMediaQueryChange(mediaQuery);
